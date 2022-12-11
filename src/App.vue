b2604c0214309c2c33671869891a33aa1bd05422
김재만 <lovesay00@naver.com>
2022-12-11 21:54:21 +0900

    20221211_kjm(vue ajax, css databinding)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,155 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <ContainerVue :data="data" />
+
+  <div class="align-center">
+    <div v-if="Lastdata">마지막 데이터 입니다.</div>
+    <br/>
+    <button @click="more" class="blue-btn">더보기</button>
+  </div>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+ </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from './components/Container.vue';
+import data from './assets/vuestargramdata';
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      data : data,
+      count : 0,
+      Lastdata : false
+    }
+  },
   components: {
-    HelloWorld
-  }
+    ContainerVue : Container
+  },
+  methods : {
+    more() {
+      axios.get(`https://codingapple1.github.io/vue/more${this.count}.json`)
+      .then((결과)=>{
+        console.log(결과.data)
+        this.data.push(결과.data)
+        this.count++
+        console.log(this.count)
+      })
+      .catch((err)=>{
+        console.log(err)
+        this.Lastdata = true
+      })
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
   margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+
+.align-center{
+  text-align: center;
+  margin: auto;
+}
+
+.blue-btn{
+  background-color: rgba(56, 96, 215, 0.65);
+  color:#eee;
+  padding : 3px;
+  border: solid 1px blue ;
+  border-radius: 7%;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <PostVue v-for="(a,i) in data" :key="i" :a="a"/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+
+export default {
+  name : 'Container',
+  components : {
+    PostVue : Post
+  },
+  props : {
+    data : Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
