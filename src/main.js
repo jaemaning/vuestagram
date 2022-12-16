d0b728dc9071aad5942c12d892cc2faa94c4458f
김재만 <lovesay00@naver.com>
2022-12-16 19:49:13 +0900

    20221216_kjm(vue_vuex 설치 및 세팅)

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ let emitter = mitt();  // mitt 라이브러리 셋팅
 let app = createApp(App)
 app.config.globalProperties.emitter = emitter; // 글로벌 변수 보관함
 
-app.mount('#app')
+import store from './store.js' // vuex4 main.js 세팅
+
+app.use(store).mount('#app')
 
